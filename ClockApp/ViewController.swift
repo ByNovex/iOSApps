9bcbf249ca7667254ecc5fab8dd993827a936871
Byron Bacusoy Pinela <ByronBP@macbook-pro-de-byron.local>
2017-01-26 22:43:32 +0100

    just a clock

--- ClockApp/ViewController.swift
+++ ClockApp/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //pruebaaaaaa
-        //pruebaaaaa22
+        
+        textLabel.text = ""
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+        
+    }
+    
+    func updateClock(){
+        
+        let date = NSDate()
+        let calendar = NSCalendar.current
+        
+        let components = calendar.dateComponents([.hour, .minute, .second], from: date as Date)
+        
+        //Como lo pondremos en modo 12h y no 24 tenemos que hacer esto
+        let hour = components.hour! > 12 ? components.hour! - 12 : components.hour!
+        
+        let hourString = hour > 9 ? "\(hour)" : "0\(hour)"
+        let minuteString = components.minute! > 9 ? "\(components.minute!)" : "0\(components.minute!)"
+        let secondString = components.second! > 9 ? "\(components.second!)" : "0\(components.second!)"
+        let am = components.hour! > 12 ? "PM" : "AM"
+        
+        let finalString = "\(hourString):\(minuteString):\(secondString) \(am)"
+        
+        textLabel.text = finalString
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
